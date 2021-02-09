78542d5718fdd966b1d33b7f4962adfe7f9bda82
Saad Shamim <saadshamim95@gmail.com>
2021-02-10 01:55:25 +0530

    Section 7 completed

---
+++ pics/src/Components/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import SearchBar from "./SearchBar";
+
+class App extends Component {
+  onSearchSubmit = (term) => {
+    console.log(term);
+  };
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: "10px" }}>
+        <SearchBar ReturnValue={this.onSearchSubmit} />
+      </div>
+    );
+  }
+}
+
+export default App;
