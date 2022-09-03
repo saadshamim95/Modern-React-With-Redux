c758f328044b6c2a4b67a269ef1652043461f1d5
Saad Shamim <saadshamim95@gmail.com>
2022-09-03 06:00:03 +0530

    Added Navigation

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import Accordion from './components/Accordion';
 import Dropdown from './components/Dropdown';
+import Route from './components/Route';
 import Search from './components/Search';
 import Translate from './components/Translate';
+import Header from './components/Header';
 
 const items = [
     {
@@ -35,10 +37,27 @@ const options = [
 ]
 
 export default () => {
-    
+    const [selected, setSelected] = useState(options[0]);
     return (
         <div>
-           <Translate />
+            <Header />
+            <Route path="/">
+                <Accordion items={items} />
+            </Route>
+            <Route path="/list">
+                <Search />
+            </Route>
+            <Route path="/dropdown">
+                <Dropdown
+                    label="Select a color"
+                    options={options}
+                    selected={selected}
+                    onSelectedChange={setSelected}
+                />
+            </Route>
+            <Route path="/translate">
+                <Translate />
+            </Route>
         </div>
     );
 }
